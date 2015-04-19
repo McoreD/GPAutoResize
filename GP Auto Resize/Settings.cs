9038b0221baa81ca096f057ea6beadc369787d3b
Michael Delpach <mcored@gmail.com>
2015-04-19 16:52:08 +0800

    Parallel.ForEach method

--- GP Auto Resize/Settings.cs
+++ GP Auto Resize/Settings.cs
@@ -1,4 +1,32 @@
-﻿using ShareX.HelpersLib;
+﻿#region License Information (GPL v3)
+
+/*
+    Google+ Auto Resize - A program that allows you to resize photos larger than 
+    2048 pixels (by default) in width or height so that it does not count towards
+    your Google Drive storage.
+    
+    Copyright (C) 2015 ShareX Developers
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+
+#endregion License Information (GPL v3)
+
+using ShareX.HelpersLib;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
